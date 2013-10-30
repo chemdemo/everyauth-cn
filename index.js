0ac0eb98e1849aa4c87cd145e5319089a09edb16
chemdemo <yangdemo@gmail.com>
2013-10-31 01:32:49 +0800

    up

---
+++ index.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var fs = require('fs');
+
+var seq = require('node-seq');
+var flow = require('flow');
+
+// app.use(require('social-oauth')(conf))
+exports = module.exports = function socialOAuth(options) {
+	var conf = options.conf || require(__dirname + '/lib/config-default');
+
+	// TODO
+	return function(req, res, next) {
+		// This middleware must be set after connect.session
+		if(!req.session) return next();
+	}
+};
+
+function fetchUser() {
+	;
+};
+
+function setLocals(req, res, alias) {
+	;
+};
