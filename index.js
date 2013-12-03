bde13858c4a9c2d236d977112f4c4ff79e5cce3a
chemdemo <yangdemo@gmail.com>
2013-12-04 02:03:54 +0800

    dev

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ var path = require('path');
 var fs = require('fs');
 var everyauth = require('./node_modules/everyauth');
 
-var socialoauth = module.exports = everyauth;
+// var socialoauth = module.exports = everyauth;
 
 var files = fs.readdirSync(__dirname + '/lib/modules');
 var includes = files.filter(function(file) {return path.basename(file, '.js');});
 
 includes.forEach(function(name) {
-    Object.defineProperty(socialoauth, name, {
-        get: function(name) {
+    Object.defineProperty(everyauth, name, {
+        get: (function(name) {
             return function() {
-                var mod = this.modules[name] || this.modules[name] = require('lib/modules/' + name);
-                if(!mod.socialoauth) mod.socialoauth = this;
+                var mod = everyauth.modules[name] || everyauth.modules[name] = require('lib/modules/' + name);
+                if(!mod.everyauth) mod.everyauth = everyauth;
                 if(mod.shouldSetup) {
-                    this.enabled[name] = mod;
+                    everyauth.enabled[name] = mod;
                 }
                 return mod;
             };
-        }(name)
+        })(name)
     });
 });
+
+module.exports = everyauth;
