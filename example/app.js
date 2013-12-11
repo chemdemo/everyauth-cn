2c798fff957b64fb47608ade79b563852499f190
chemdemo <yangdemo@gmail.com>
2013-12-12 03:08:26 +0800

    update example app

--- example/app.js
+++ example/app.js
@@ -31,7 +31,7 @@ app.get('/', function(req, res, next) {
 });
 
 app.get('/user', function(req, res, next) {
-    red.render('user');
+    res.render('user');
 });
 
 app.listen(app.get('port'), function() {
